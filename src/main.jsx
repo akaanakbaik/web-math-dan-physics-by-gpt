5d9509d8ab3bd3d9e6489e024127a853ef1d5246
aka <khaliqarrasyidabdul@gmail.com>
2026-05-10 07:13:21 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "sonner";
 import App from "./App.jsx";
+import ErrorBoundary from "@/components/system/ErrorBoundary";
 import "./index.css";
+
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <Toaster richColors closeButton position="top-right" expand visibleToasts={6} />
+    <ErrorBoundary>
+      <App />
+      <Toaster richColors closeButton position="top-right" expand visibleToasts={6} />
+    </ErrorBoundary>
   </React.StrictMode>
 );
